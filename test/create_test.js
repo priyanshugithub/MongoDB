42de114f139902a8ca1448240571b043a9d2b0c5
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-04-26 22:22:11 +0530

    Mongodb practice

---
+++ test/create_test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Creating records', () => {
+    it('It saves a user', () => {
+        const joe = new User({ name : 'Joe' });
+
+        joe.save()
+           .then(() => {
+               //Has Joe been saved successfully?
+               assert(!joe.isNew);
+               done();
+           });
+
+    });
+});
