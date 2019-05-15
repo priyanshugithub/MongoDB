5d111d704e19423c2b2971338505b8f61c408d9b
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-15 23:45:45 +0530

    Validating Records

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({ name: 'Joe' });
+        joe = new User({ name: 'Joe', postCount: 0 });
         joe.save()
            .then(() => done());
     });
@@ -48,5 +48,13 @@ describe('Updating records', () => {
             User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
             done
             );
+    });
+    it('A user can have their post count incremented by 1', (done) => {
+        User.update({ name: 'Joe' }, { $inc: { postCount: 10 } })
+        .then(() => User.findOne({ name: 'Joe' }))
+        .then((user) => {
+            assert(user.postCount === 10);
+            done();
+        });
     });
 });
