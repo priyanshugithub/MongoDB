bf0aecf75bf2b45b2afc367dd37f1833d75896da
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-19 19:05:38 +0530

    Validation

--- test/validation_test.js
+++ test/validation_test.js
@@ -9,3 +9,23 @@ describe('Validating records', () => {
 
     assert(message === 'Name is required.');
   });
+
+  it('requires a user\'s name longer than 2 characters', () => {
+    const user = new User({ name: 'Al' });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name must be longer than 2 characters.');
+  });
+
+  it('disallows invalid records from being saved', (done) => {
+    const user = new User({ name: 'Al' });
+    user.save()
+      .catch((validationResult) => {
+        const { message } = validationResult.errors.name;
+
+        assert(message === 'Name must be longer than 2 characters.');
+        done();
+      });
+  });
+});
