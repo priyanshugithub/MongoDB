58ea5b752cb2ddfc6495c213de375eac1fdd84b6
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-29 23:30:27 +0530

    Removing subdocuments

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -44,7 +44,14 @@ describe('Subdocuments', () => {
         joe.save()
            .then(() => User.findOne({ name: 'Joe' }))
            .then((user) => {
-                
+               const post = user.posts[0];
+               post.remove();
+               return user.save();
+           })
+           .then(() => User.findOne({ name : 'Joe' }))
+           .then((user) => {
+            assert(user.posts.length === 0);
+            done();
            });
     });
 });
