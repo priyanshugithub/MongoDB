a1c0a86c22448c43c74059e2119225c4232afb5e
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-29 23:45:02 +0530

    Defining Virtual Types

--- src/user.js
+++ src/user.js
@@ -3,17 +3,34 @@ const PostSchema = require('./post');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        validate: {
-            validator: (name) => name.length > 2,
-            message: 'Name must be longer than 2 characters'
-        },
-        required: [true, 'Name is required.']
+  name: {
+    type: String,
+    validate: {
+      validator: (name) => name.length > 2,
+      message: 'Name must be longer than 2 characters.'
     },
-    postCount: Number,
-    posts: [PostSchema]
+    required: [true, 'Name is required.']
+  },
+   posts: [PostSchema]
+//,
+//   likes: Number,
+//   blogPosts: [{
+//     type: Schema.Types.ObjectId,
+//     ref: 'blogPost'
+//   }]
 });
+
+UserSchema.virtual('postCount').get(function() {
+  console.log('Hi');
+});
+
+// UserSchema.pre('remove', function(next) {
+//   const BlogPost = mongoose.model('blogPost');
+//   // this === joe
+
+//   BlogPost.remove({ _id: { $in: this.blogPosts } })
+//     .then(() => next());
+// });
 
 const User = mongoose.model('user', UserSchema);
 
